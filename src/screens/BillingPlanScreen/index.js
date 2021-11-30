3a40e990a792c1fbd483dde531d9e99581f709dd
sagar9297 <40600831+sagar9297@users.noreply.github.com>
2021-11-30 19:14:34 +0530

    Screens Added

---
+++ src/screens/BillingPlanScreen/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const index = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>This is billing plan screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default index;
