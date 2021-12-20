6ca09d86131584dfc70b88f0f9cf83f61a467986
sagar9297 <40600831+sagar9297@users.noreply.github.com>
2021-12-20 21:16:59 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import AppNavigator from "./src/routes/AppNavigator";
 import { MapScreen } from "./src/screens";
 import Screen from "./src/components/Screen";
 import DrawerStack from "./src/routes/DrawerStack";
+import AppTextInput from "./src/components/AppTextInput";
 
 export default function App() {
   const [loaded] = useFonts({
@@ -21,15 +22,25 @@ export default function App() {
     return <AppLoading />;
   } else {
     return (
-      // <Provider store={store}>
-      //   <AppNavigator />
-      // </Provider>
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
       // <NavigationContainer>
       //   <DrawerStack />
       // </NavigationContainer>
-      <Screen>
-        <MapScreen />
-      </Screen>
+      // <Screen>
+      //   {/* <MapScreen /> */}
+      //   <AppTextInput
+      //     placeholder="Enter name here"
+      //     rightIcon="check"
+      //     leftIcon="circle"
+      //     style={{
+      //       borderWidth: 2,
+      //       borderRadius: 25,
+      //       padding: 20,
+      //     }}
+      //   />
+      // </Screen>
     );
   }
 }
