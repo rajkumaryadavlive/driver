0045ef4f1bde88fff7ee6489ded0736018f292e9
sagar9297 <40600831+sagar9297@users.noreply.github.com>
2021-12-21 20:20:52 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { store } from "./src/store/store";
 import AppNavigator from "./src/routes/AppNavigator";
-import { MapScreen } from "./src/screens";
+import { MapScreen, SignUp } from "./src/screens";
 import Screen from "./src/components/Screen";
 import DrawerStack from "./src/routes/DrawerStack";
 import AppTextInput from "./src/components/AppTextInput";
@@ -22,25 +22,16 @@ export default function App() {
     return <AppLoading />;
   } else {
     return (
-      <Provider store={store}>
-        <AppNavigator />
-      </Provider>
+      // <Provider store={store}>
+      //   <AppNavigator />
+      // </Provider>
       // <NavigationContainer>
       //   <DrawerStack />
       // </NavigationContainer>
-      // <Screen>
-      //   {/* <MapScreen /> */}
-      //   <AppTextInput
-      //     placeholder="Enter name here"
-      //     rightIcon="check"
-      //     leftIcon="circle"
-      //     style={{
-      //       borderWidth: 2,
-      //       borderRadius: 25,
-      //       padding: 20,
-      //     }}
-      //   />
-      // </Screen>
+      <Screen>
+        <MapScreen />
+        {/* <SignUp /> */}
+      </Screen>
     );
   }
 }
